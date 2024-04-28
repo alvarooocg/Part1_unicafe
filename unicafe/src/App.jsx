13de163f29901308deb1484d406354d8dbcf5d07
alvarocg <103962133+alvarooocg@users.noreply.github.com>
2024-04-28 12:20:35 +0200

    Finish Part 1.6

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
 
-function App() {
-  const [good, serGood] = useState(0)
+const App = () => {
+  const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      <Header text="give feedback"/>
+      <Button text="good" handleClick={() => setGood(good + 1)}/>
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)}/>
+      <Button text="bad" handleClick={() => setBad(bad + 1)}/>
+      <Header text="statics"/>
+      <Statistic name="good" count={good}/>
+      <Statistic name="neutral" count={neutral}/>
+      <Statistic name="bad" count={bad}/>
     </div>
   )
 }
+
+const Header = (props) => (
+    <h1>
+      {props.text}
+    </h1>
+)
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistic = (props) => (
+  <div>
+    {props.name} {props.count}
+  </div>
+)
 
 export default App
